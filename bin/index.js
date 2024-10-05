a4d342eef8ef8ab5c5c4c8b872fc6b65aad14201
cosuoji <56638390+cosuoji@users.noreply.github.com>
2024-10-05 17:05:25 +0100

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -58,26 +58,5 @@ program
   })
 
 
-// program
-//   .command('build')
-//   .description('build web site for deployment')
-//   .action(() => {
-//     console.log('build');
-//   });
-
-// program
-//   .command('deploy')
-//   .description('deploy web site to production')
-//   .action(() => {
-//     console.log('deploy');
-//   });
-
-// program
-//   .command('serve', { isDefault: true })
-//   .description('launch web server')
-//   .option('-p,--port <port_number>', 'web port')
-//   .action((options) => {
-//     console.log(`server on port ${options.port}`);
-//   });
 
 program.parse(process.argv);
